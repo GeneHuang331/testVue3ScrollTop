c8c0744ffaa90aff60c1c52969ae31156f1e794b
Gene Huang <tutor8110221510@gmail.com>
2024-10-14 11:17:45 +0800

    scrolltop

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,10 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-//  scrollBehavior(to, from, savedPosition) {
-//     // 返回到顶部
-//     return { top: 0 };
-//   },
+ scrollBehavior(to, from, savedPosition) {
+    // 返回到顶部
+    return { top: 0 };
+  },
   routes: [
     {
       path: '/',
