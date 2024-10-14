885e1424c2497164295039ae94062e1822a5062a
Gene Huang <tutor8110221510@gmail.com>
2024-10-14 10:56:42 +0800

    scrolltop

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+//  scrollBehavior(to, from, savedPosition) {
+//     // 返回到顶部
+//     return { top: 0 };
+//   },
   routes: [
     {
       path: '/',
@@ -20,4 +24,12 @@ const router = createRouter({
   ]
 })
 
+
+router.beforeEach((to, from) => {
+ console.log('123');
+
+})
+
 export default router
+
+
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,11 +5,18 @@
 </template>
 
 <style>
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    height: 5000px;
+  }
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
+    height: 5000px;
   }
 }
 </style>
